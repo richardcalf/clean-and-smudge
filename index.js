0f4b2b6a71aea800d323cd32bc5eaa810af9c2b9
Richard Calf <richard.calf@gmail.com>
2020-05-30 17:07:57 +0200

    added index.js, along with instructions file

---
+++ index.js
@@ -0,0 +1,8 @@
+const API_KEY = "{SECURE_API_KEY}";
+const makeAPICall = function(key) {
+	console.log("Making API Call....");
+	console.log("code change in origin master");
+}
+makeAPICall(API_KEY);
+
+
